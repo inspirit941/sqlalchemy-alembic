acf6ec5ab2b45fb51cf227638f04985e00a679c8
inspirit941 <inspirit941@gmail.com>
2024-01-26 14:22:06 +0900

    feat: alembic의 env.py 설정

--- README.md
+++ README.md
@@ -26,4 +26,6 @@ python -m alembic init migrations
 위 방식은 sync. async 쓰려면 다른 방법 적용해야 함.
 - 환경설정 파일은 alembic.ini 가 기본. but 보통은 ini 파일에 직접 넣기보다는 env.py 를 쓴다. 
   - 코드에 직접 DB 설정값을 넣을 필요가 없기 때문.
-- 
+
+Object로 만들어진 SQL문과 실제 DB 비교 -> DDL 생성.
+- 변경사항을 자동 감지해주는 건 아님. 예컨대 컬럼 타입을 바꾼다거나 컬럼명을 바꿀 때... 자동으로 해주진 않음. manually.
--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,13 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config, URL
 from sqlalchemy import pool
 
 from alembic import context
+
+# from environs import Env
+# env = Env()
+# env.read_env('.prod.env') # path 정해서 phase 구분할 수 있다.
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,11 +18,29 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+# pw 등은 .env로 정의해두고 환경변수로 받아쓰는 게 낫다. 귀찮으므로 여기선 그냥 진행
+url = URL.create(
+    drivername="postgresql+psycopg2",
+    username="testuser",
+    password="testpassword",
+    host="localhost",
+    port=5432,
+    database="testuser",
+)
+
+config.set_main_option(
+    "sqlalchemy.url",
+    url
+)
+
 # add your model's MetaData object here
+# -> DB table 생성에 쓸 metadata 설정 가능. Base 모델 적용할 수 있다.
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+import lesson_2
+target_metadata = lesson_2.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
